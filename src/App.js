65c0b76ae0d28324f53455ed5137df6df4422f3f
petudeveloper <davidalvarezmazzo@gmail.com>
2021-12-05 17:15:53 -0500

    Update general sizing and spacing.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import NavBar from './components/NavBar/NavBar';
 import Introduction from './components/Introduction/Introduction';
 import AboutMe from './components/AboutMe/AboutMe';
+import Work from './components/Work/Work';
 
 const App = () => (
   <div className="App">
@@ -8,6 +9,7 @@ const App = () => (
     <div className="content">
       <Introduction />
       <AboutMe />
+      <Work />
     </div>
   </div>
 );
---
+++ src/components/Work/Work.js
@@ -0,0 +1,12 @@
+import styles from './work.module.css';
+
+const Work = () => (
+  <section className={styles.container}>
+    <h2 className={styles.numberedHeading}>Where I’ve Worked</h2>
+    <div className={styles.content}>
+      this is the content
+    </div>
+  </section>
+);
+
+export default Work;
